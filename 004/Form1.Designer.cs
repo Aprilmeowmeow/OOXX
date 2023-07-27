0e73e84b77f184caf31e41ccfc5bdc4520868a6e
April <linda884015@gamil.com>
2023-07-27 14:19:54 +0800

    init OOXX game with nine button

---
+++ 004/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace _004
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.檔案ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開始玩遊戲ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.離開ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.說明ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.OX3 = new System.Windows.Forms.Button();
+            this.OX1 = new System.Windows.Forms.Button();
+            this.OX2 = new System.Windows.Forms.Button();
+            this.OX4 = new System.Windows.Forms.Button();
+            this.OX5 = new System.Windows.Forms.Button();
+            this.OX6 = new System.Windows.Forms.Button();
+            this.OX7 = new System.Windows.Forms.Button();
+            this.OX8 = new System.Windows.Forms.Button();
+            this.OX9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.StartGame = new System.Windows.Forms.Button();
+            this.EndGame = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.檔案ToolStripMenuItem,
+            this.說明ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(930, 38);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 檔案ToolStripMenuItem
+            // 
+            this.檔案ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開始玩遊戲ToolStripMenuItem,
+            this.離開ToolStripMenuItem});
+            this.檔案ToolStripMenuItem.Name = "檔案ToolStripMenuItem";
+            this.檔案ToolStripMenuItem.Size = new System.Drawing.Size(53, 23);
+            this.檔案ToolStripMenuItem.Text = "檔案";
+            // 
+            // 開始玩遊戲ToolStripMenuItem
+            // 
+            this.開始玩遊戲ToolStripMenuItem.Name = "開始玩遊戲ToolStripMenuItem";
+            this.開始玩遊戲ToolStripMenuItem.Size = new System.Drawing.Size(167, 26);
+            this.開始玩遊戲ToolStripMenuItem.Text = "開始玩遊戲";
+            // 
+            // 離開ToolStripMenuItem
+            // 
+            this.離開ToolStripMenuItem.Name = "離開ToolStripMenuItem";
+            this.離開ToolStripMenuItem.Size = new System.Drawing.Size(167, 26);
+            this.離開ToolStripMenuItem.Text = "離開";
+            // 
+            // 說明ToolStripMenuItem
+            // 
+            this.說明ToolStripMenuItem.Name = "說明ToolStripMenuItem";
+            this.說明ToolStripMenuItem.Size = new System.Drawing.Size(53, 23);
+            this.說明ToolStripMenuItem.Text = "說明";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 128F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 137F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 127F));
+            this.tableLayoutPanel1.Controls.Add(this.OX3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.OX1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.OX2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.OX4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.OX5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.OX6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.OX7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.OX8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.OX9, 2, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(122, 78);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.30556F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.69444F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 131F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(392, 418);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.OX3.Location = new System.Drawing.Point(268, 3);
+            this.OX3.Name = "button3";
+            this.OX3.Size = new System.Drawing.Size(121, 127);
+            this.OX3.TabIndex = 2;
+            this.OX3.Text = "button3";
+            this.OX3.UseVisualStyleBackColor = true;
+            this.OX3.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button1
+            // 
+            this.OX1.Location = new System.Drawing.Point(3, 3);
+            this.OX1.Name = "button1";
+            this.OX1.Size = new System.Drawing.Size(122, 127);
+            this.OX1.TabIndex = 2;
+            this.OX1.Text = "button1";
+            this.OX1.UseVisualStyleBackColor = true;
+            this.OX1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.OX2.Location = new System.Drawing.Point(131, 3);
+            this.OX2.Name = "button2";
+            this.OX2.Size = new System.Drawing.Size(126, 127);
+            this.OX2.TabIndex = 3;
+            this.OX2.Text = "button2";
+            this.OX2.UseVisualStyleBackColor = true;
+            this.OX2.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button4
+            // 
+            this.OX4.Location = new System.Drawing.Point(3, 144);
+            this.OX4.Name = "button4";
+            this.OX4.Size = new System.Drawing.Size(122, 130);
+            this.OX4.TabIndex = 4;
+            this.OX4.Text = "button4";
+            this.OX4.UseVisualStyleBackColor = true;
+            this.OX4.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button5
+            // 
+            this.OX5.Location = new System.Drawing.Point(131, 144);
+            this.OX5.Name = "button5";
+            this.OX5.Size = new System.Drawing.Size(126, 130);
+            this.OX5.TabIndex = 5;
+            this.OX5.Text = "button5";
+            this.OX5.UseVisualStyleBackColor = true;
+            this.OX5.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button6
+            // 
+            this.OX6.Location = new System.Drawing.Point(268, 144);
+            this.OX6.Name = "button6";
+            this.OX6.Size = new System.Drawing.Size(121, 130);
+            this.OX6.TabIndex = 6;
+            this.OX6.Text = "button6";
+            this.OX6.UseVisualStyleBackColor = true;
+            this.OX6.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button7
+            // 
+            this.OX7.Location = new System.Drawing.Point(3, 289);
+            this.OX7.Name = "button7";
+            this.OX7.Size = new System.Drawing.Size(122, 119);
+            this.OX7.TabIndex = 7;
+            this.OX7.Text = "button7";
+            this.OX7.UseVisualStyleBackColor = true;
+            this.OX7.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button8
+            // 
+            this.OX8.Location = new System.Drawing.Point(131, 289);
+            this.OX8.Name = "button8";
+            this.OX8.Size = new System.Drawing.Size(131, 119);
+            this.OX8.TabIndex = 8;
+            this.OX8.Text = "button8";
+            this.OX8.UseVisualStyleBackColor = true;
+            this.OX8.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button9
+            // 
+            this.OX9.Location = new System.Drawing.Point(268, 289);
+            this.OX9.Name = "button9";
+            this.OX9.Size = new System.Drawing.Size(121, 119);
+            this.OX9.TabIndex = 9;
+            this.OX9.Text = "button9";
+            this.OX9.UseVisualStyleBackColor = true;
+            this.OX9.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(266, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "OX";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button10
+            // 
+            this.StartGame.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.StartGame.Location = new System.Drawing.Point(58, 514);
+            this.StartGame.Name = "button10";
+            this.StartGame.Size = new System.Drawing.Size(177, 51);
+            this.StartGame.TabIndex = 3;
+            this.StartGame.Text = "開始遊戲";
+            this.StartGame.UseVisualStyleBackColor = true;
+            this.StartGame.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.EndGame.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.EndGame.Location = new System.Drawing.Point(351, 514);
+            this.EndGame.Name = "button11";
+            this.EndGame.Size = new System.Drawing.Size(203, 51);
+            this.EndGame.TabIndex = 4;
+            this.EndGame.Text = "結束遊戲";
+            this.EndGame.UseVisualStyleBackColor = true;
+            this.EndGame.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 577);
+            this.Controls.Add(this.EndGame);
+            this.Controls.Add(this.StartGame);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 檔案ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開始玩遊戲ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 離開ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 說明ToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button OX1;
+        private System.Windows.Forms.Button OX3;
+        private System.Windows.Forms.Button OX2;
+        private System.Windows.Forms.Button OX4;
+        private System.Windows.Forms.Button OX5;
+        private System.Windows.Forms.Button OX6;
+        private System.Windows.Forms.Button OX7;
+        private System.Windows.Forms.Button OX8;
+        private System.Windows.Forms.Button OX9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button StartGame;
+        private System.Windows.Forms.Button EndGame;
+    }
+}
+
---
+++ 004/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _004
+{
+    public partial class Form1 : Form
+    {
+        Button[] BtnControls = new Button[9];
+        bool isGameOver = false;
+        bool isO = true;
+        private static readonly int[,] WinGroup = new int[8, 3]
+        {
+            {0,1,2},
+            {3,4,5},
+            {6,7,8},
+            {0,3,6},
+            {1,4,7},
+            {2,5,8},
+            {0,4,8},
+            {2,4,6}
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            BtnControls = new Button[9]
+            {
+                OX1, OX2, OX3, OX4, OX5, OX6, OX7, OX8, OX9
+            };
+        }
+        
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Button tmpButton = (Button)sender;
+            if (isGameOver)
+            {
+                MessageBox.Show("遊戲結束....請重新開始遊戲!", "遊戲結束", MessageBoxButtons.OK);
+                return;
+            }
+            if(tmpButton.Text != "")
+            {
+                MessageBox.Show("這個按鍵已經選擇了，請點選其他位置！", "提示", MessageBoxButtons.OK);
+                return;
+            }
+            if (isO)
+            {
+                tmpButton.Text = "O";
+                tmpButton.BackColor = Color.LightPink;
+                timer1.Start();
+            }
+            else
+            {
+                tmpButton.Text = "X";
+                tmpButton.BackColor = Color.LightBlue;
+            }
+
+            isO = !isO;
+
+            bool[] GameStatus = CheckWinGrop(BtnControls);
+            isGameOver = GameStatus[1];//someone win
+
+            if (GameStatus[0])
+            {
+                DialogResult result = MessageBox.Show("遊戲結束....\r\n" + tmpButton.Text + " 獲勝\r\n是否重新開始遊戲", "遊戲結束", MessageBoxButtons.OKCancel);
+                if (result == DialogResult.OK)
+                    initButtons();
+                return;
+            }
+            if (GameStatus[1])
+            {
+                DialogResult result = MessageBox.Show("遊戲結束....\r\n和局\r\n是否重新開始遊戲", "遊戲結束", MessageBoxButtons.OKCancel);
+                if (result == DialogResult.OK)
+                    initButtons();
+            }
+        }
+
+        private void initButtons()
+        {
+            isGameOver = false;
+            isO = true;
+            for(int i = 0; i < BtnControls.Length; i++)
+            {
+                BtnControls[i].Text = "";
+                BtnControls[i].BackColor = Color.LightPink;
+                BtnControls[i].Font = new Font("Arial", 50, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((System.Byte)1));
+                BtnControls[i].Click += new EventHandler(this.button1_Click_1);
+            }
+        }
+
+        private bool[] CheckWinGrop(Button[] btnControls)
+        {
+            bool[] gameWinOver = new bool[2] {false, false};
+            int btnIsUse = 1;
+            for(int i = 0; i<8; i++)
+            {
+                int a = WinGroup[i, 0];
+                int b = WinGroup[i, 1];
+                int c = WinGroup[i, 2];
+                Button b1 = btnControls[a];
+                Button b2 = btnControls[b];
+                Button b3 = btnControls[c];
+
+                if (b1.Text == "" || b2.Text == "" || b3.Text == "")
+                    continue;
+                if (b1.Text == b2.Text && b3.Text == b2.Text)
+                {
+                    b1.BackColor = b2.BackColor = b3.BackColor = Color.LightPink;
+                    b1.Font = b2.Font = b3.Font = new System.Drawing.Font("Times New Roman", 35, System.Drawing.FontStyle.Italic & System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((System.Byte)1));
+                    gameWinOver = new bool[2] { true, true };
+                    break;
+                }
+                if (btnControls[i].Text != "")
+                {
+                    btnIsUse++;
+                    if (btnIsUse == 9)
+                    {
+                        gameWinOver[1] = true;
+                    }
+                }
+                
+            }
+            return gameWinOver;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                if (!isO && BtnControls[i].Text == "")
+                {
+                    BtnControls[i].Text = "X";
+                    BtnControls[i].BackColor = Color.LightYellow;
+                    isO = !isO;
+                    timer1.Stop();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initButtons();
+            timer1.Stop();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < BtnControls.Length ; i++)
+            {
+                BtnControls[i].Text = "";
+                BtnControls[i].BackColor = Color.LightPink;
+                BtnControls[i].Font = new Font("Arial", 50, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((System.Byte)1));
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
